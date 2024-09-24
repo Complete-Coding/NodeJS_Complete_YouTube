bbfddf933e4ab3724db24fa7812eced977291c9b
Prashant Jain <prashant@knowledgegate.in>
2024-09-24 09:16:44 +0530

    Leacture-4

--- Lecture 4 - Request and Reponse/user.js
+++ Lecture 4 - Request and Reponse/user.js
@@ -9,24 +9,23 @@ const server = http.createServer((req, res) => {
     res.write('<html>');
     res.write('<head><title>Complete Coding</title></head>');
     res.write('<body><h1>Enter Your Details:</h1>');
-    res.write('<form method="POST" action="submit-details">');
+    res.write('<form action="/submit-details" method="POST">');
     res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
-    res.write('<input type="radio" name="gender" id="male" value="male" />');
     res.write('<label for="male">Male</label>')
-    res.write('<input type="radio" name="gender" id="female" value="female" />');
-    res.write('<label for="female">Female</label>');
-    res.write('<br><button type="submit">Submit</button>');
+    res.write('<input type="radio" id="male" name="gender" value="male" />')
+    res.write('<label for="female">Female</label>')
+    res.write('<input type="radio" id="female" name="gender" value="female" />')
+    res.write('<br><input type="submit" value="Submit">');
     res.write('</form>');
     res.write('</body>');
     res.write('</html>');
     return res.end();
-  
-  } else if (req.url === '/submit-details' &&
-             req.method === 'POST') {
+
+  } else if (req.url.toLowerCase() === "/submit-details" &&
+        req.method == "POST") {
     fs.writeFileSync('user.txt', 'Prashant Jain');
     res.statusCode = 302;
     res.setHeader('Location', '/');
-    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');

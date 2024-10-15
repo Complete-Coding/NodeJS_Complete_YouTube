3eac1dae410d143315d9374e8b1200a84e5aec0a
Prashant Jain <prashant@knowledgegate.in>
2024-10-15 21:04:53 +0530

    chapter 11

---
+++ Chapter 11 - airbnb styling/app.js
@@ -0,0 +1,27 @@
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+
+//Local Module
+const userRouter = require("./routes/userRouter")
+const hostRouter = require("./routes/hostRouter")
+const rootDir = require("./utils/pathUtil");
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
